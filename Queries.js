22bb3aa63c4c1a2a2513359da094326853b346c6
mar <rojanomariano@gmail.com>
2023-02-13 07:10:50 -0800

    Queries for All adding to database

--- Queries.js
+++ Queries.js
@@ -17,6 +17,25 @@ class Queries {
 
     viewAllEmployees() {
         return this.connection.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;`
-        )
+        );
+    }
+
+
+    addDepartment (departmentName) {
+        return this.connection
+        .promise()
+        .query("INSERT INTO DEPARTMENT (name) VALUES (?)", [departmentName]);
+    }
+
+    addRole(title, salary, departmentChoices) {
+        return this.connection
+        .promise()
+        .query("INSERT INTO role (title, salary, department_id) VALUES (?,?,?)", [title, salary, departmentChoices]);
+    }
+
+    addEmployee(first_name, last_name, role, manager) {
+        return this.connection
+        .promise()
+        .query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)", [first_name, last_name, role, manager]);
     }
 }
