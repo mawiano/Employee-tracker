5e9dfe06fd4a035cfa4294b66d81619475f3f8a4
mar <rojanomariano@gmail.com>
2023-02-13 04:24:34 -0800

    Created is statments for choices and answers

--- index.js
+++ index.js
@@ -8,14 +8,51 @@ const choices = [
         type: "list",
         message: "What would you like to do?",
         options: [
-            "View all Employees",
-            "Add Employee",
-            "Update Employee Role",
-            "View All Roles",
-            "Add Role",
-            "Add Department",
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add a department",
+            "add a role",
+            "add an employee",
+            "update an employee role",
+            "Restart",
         ];
         name: "choiceSelection",
     },
 ];
-
+// view all departments, view all roles, view all employees, add a department, add a role,
+//  add an employee, and update an employee role
+function init() {
+    askPromptQuestions();
+}
+    function askPromptQuestions() {
+        inquirer.prompt(choices)
+        // If statements for every choice they pick
+        .then((answers) => {
+            if(answers.choiceSelection === "view all departments") {
+                viewAllDepartments();
+            }
+            if(answers.choiceSelection === "view all roles") {
+                viewAllRoles();
+            }
+            if(answers.choiceSelection === "view all employees") {
+                viewAllEmployees();
+            }
+            if(answers.choiceSelection === "add a department") {
+                addDepartment();
+            }
+            if(answers.choiceSelection === "add a role") {
+                addRole();
+            }
+            if(answers.choiceSelection === "dd an employee") {
+                addEmployee();
+            }
+            if(answers.choiceSelection === "update an employee role") {
+                updateEmployeeRole();
+            }
+            if(answers.choiceSelection === "Restart") {
+                return askPromptQuestions();
+            }
+        });
+    }
+   
