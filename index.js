6ba2b67eb913b625480033b24469fd50ec2fd6fd
mar <rojanomariano@gmail.com>
2023-02-13 04:02:34 -0800

    Added Initial chocies array

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const Queries = require ("/Queries");
+
+// Initial questions array 
+const choices = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        options: [
+            "View all Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "Add Department",
+        ];
+        name: "choiceSelection",
+    },
+];
+
